73587d23e6128122ed0d74d15aea89f8ccd4d11a
kareemhassanmahross <kareem.hassan.diap@gmail.com>
2025-12-22 19:35:32 +0200

    first commit

---
+++ src/newStart/DataType.java
@@ -0,0 +1,35 @@
+package newStart;
+
+public class DataType {
+    public static void main(String[] args) {
+        byte by = 127;
+        short sh = 32767;
+        int i = 1236985;
+        long l = 2341234123412341L;
+        float fl = 1234.2341234F;
+        double dbl = 2341.2341234;
+        char ch = 'k';
+        String name  = "kareem";
+        System.out.println("The Value Of Byte Variable is " + by);
+        System.out.println("The Value Of Short Variable is " + sh);
+        System.out.println("The Value Of int Variable is " + i);
+        System.out.println("The Value Of long Variable is " + l);
+        System.out.println("The Value Of float Variable is " + fl);
+        System.out.println("The Value Of double Variable is " + dbl);
+        System.out.println("The Value Of Char Variable is " + ch);
+        System.out.println("The Value Of String Variable is " + name);
+
+
+        int x = 5000000;
+        System.out.println("the Int Value is " + x);
+        long z = x;
+        System.out.println("The long value is " + z);
+        long lo = 34523452345234L;
+        System.out.println("The Long Value is "+ lo);
+        int v = (int)lo;
+        System.out.println("The int new Value is "+ v );
+
+
+
+    }
+}
---
+++ src/newStart/DayOne.java
@@ -0,0 +1,12 @@
+package newStart;
+
+public class DayOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+    }
+}
